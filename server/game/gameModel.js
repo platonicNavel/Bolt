247a5e46b01ede8fd4f4a17c3e282686263e32f8
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-03-02 11:04:38 -0800

    Fully refactor gameModel.js using ES6 syntax

--- server/game/gameModel.js
+++ server/game/gameModel.js
@@ -3,10 +3,10 @@ import mongoose from 'mongoose';
 // Inside our DB, a game consists of two users, and a flag indicating that
 // the game is currently going on
 
-var GameSchema = new mongoose.Schema({
+const GameSchema = new mongoose.Schema({
   player1: Object,
   player2: Object,
-  active: Boolean
+  active: Boolean,
 });
 
 export default mongoose.model('game', GameSchema);
