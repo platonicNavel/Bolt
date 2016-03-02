322cee85c45ec8d171819f6c07c51e7a9b0d85d3
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-03-02 11:16:55 -0800

    Fully refactor userController.js using ES6 syntax

--- server/users/userController.js
+++ server/users/userController.js
@@ -4,29 +4,29 @@ import jwt from 'jwt-simple';
 import helpers from '../config/helpers';
 
 // Promisify a few mongoose methods with the `q` promise library
-var findUser = Q.nbind(User.findOne, User);
-var createUser = Q.nbind(User.create, User);
-var updateUserDB = Q.nbind(User.update, User);
+const findUser = Q.nbind(User.findOne, User);
+const createUser = Q.nbind(User.create, User);
+const updateUserDB = Q.nbind(User.update, User);
 
 export default {
 
   // Sign a user in
-  signin: function (req, res, next) {
-    var username = req.body.username;
-    var password = req.body.password;
+  signin(req, res, next) {
+    const username = req.body.username;
+    const password = req.body.password;
 
     // see if they exist...
-    findUser({username: username})
-    .then(function (user) {
+    findUser({ username: username })
+    .then((user) => {
       if (!user) {
         // ...if we can't find them, throw error
         next(new Error('User does not exist'));
       } else {
         // ...if we can, check the password
         return user.comparePasswords(password)
-        .then(function (foundUser) {
+        .then((foundUser) => {
           if (foundUser) {
-            var token = jwt.encode(user, 'secret');
+            const token = jwt.encode(user, 'secret');
             res.json({
               token: token,
               username: user.username,
@@ -36,7 +36,7 @@ export default {
               phone: user.phone,
               preferredDistance: user.preferredDistance,
               runs: JSON.stringify(user.runs),
-              achievements: JSON.stringify(user.achievements)
+              achievements: JSON.stringify(user.achievements),
             });
           } else {
             return next(new Error('No user'));
@@ -44,101 +44,100 @@ export default {
         });
       }
     })
-    .fail(function (error) {
+    .fail((error) => {
       next(error);
     });
   },
 
-  signup: function (req, res, next) {
-    var username = req.body.username;
-    var password = req.body.password;
+  signup(req, res, next) {
+    const username = req.body.username;
+    const password = req.body.password;
 
     // check to see if user already exists
-    findUser({username: username})
-    .then(function (user) {
+    findUser({ username: username })
+    .then((user) => {
       if (user) {
         next(new Error('User already exist!'));
       } else {
         // make a new user if not one
         return createUser({
           username: username,
-          password: password
+          password: password,
         });
       }
     })
-    .then(function (user) {
+    .then((user) => {
       // create token to send back for auth
-      var token = jwt.encode(user, 'secret');
-      res.json({token: token});
+      const token = jwt.encode(user, 'secret');
+      res.json({ token: token });
     })
-    .fail(function (error) {
+    .fail((error) => {
       next(error);
     });
   },
 
-  updateUser: function (req, res, next) {
+  updateUser(req, res, next) {
     // This is tied to createProfile on the frontend, so users can update
     // their info
-    var newData = req.body.newInfo;
-    var username = req.body.user.username;
-    var user = {
-      username: username
+    const newData = req.body.newInfo;
+    const username = req.body.user.username;
+    const user = {
+      username: username,
     };
 
     // search the DB for the specific user
-    var queryCondition = {username: username};
+    const queryCondition = { username: username };
 
     findUser(user)
-    .then(function (user) {
+    .then((user) => {
       if (user) {
         return updateUserDB(queryCondition, newData);
-      } else {
-        next(new Error('No user found!'));
       }
+      next(new Error('No user found!'));
     })
-    .fail(function (error) {
+    .fail((error) => {
       next(error);
     });
   },
 
-  getUser: function (req, res, next) {
-    var token = req.headers['x-access-token'];
+  getUser(req, res, next) {
+    const token = req.headers['x-access-token'];
     if (!token) {
       next(new Error('No token'));
     } else {
-      var user = jwt.decode(token, 'secret');
-      findUser({username: user.username})
-      .then(function (user) {
+      const user = jwt.decode(token, 'secret');
+      findUser({ username: user.username })
+      .then((user) => {
         res.json(user);
       })
-      .catch(function (err) {
+      .catch((err) => {
         console.error(err);
         res.send(404);
       });
     }
   },
 
-  checkAuth: function (req, res, next) {
+  checkAuth(req, res, next) {
     // checking to see if the user is authenticated
     // grab the token in the header is any
     // then decode the token, which we end up being the user object
     // check to see if that user exists in the database
-    var token = req.headers['x-access-token'];
+    const token = req.headers['x-access-token'];
     if (!token) {
       next(new Error('No token'));
     } else {
-      var user = jwt.decode(token, 'secret');
-      findUser({username: user.username})
-      .then(function (foundUser) {
+      const user = jwt.decode(token, 'secret');
+      findUser({ username: user.username })
+      .then((foundUser) => {
         if (foundUser) {
           res.send(200);
         } else {
           res.send(401);
         }
       })
-      .fail(function (error) {
+      .fail((error) => {
         next(error);
       });
     }
-  }
+  },
 };
