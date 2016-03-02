9fbf28fda5be483a77f650d8bf62b2fcf30e0f21
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-03-02 11:07:02 -0800

    Fully refactor multiGameController.js using ES6 syntax

--- server/game/multiGameController.js
+++ server/game/multiGameController.js
@@ -3,23 +3,23 @@ import Q from 'q';
 import helpers from '../config/helpers';
 
 // Define promisified versions of mongoose methods
-var findGame = Q.nbind(Game.findOne, Game);
-var createGame = Q.nbind(Game.create, Game);
-var updateGame = Q.nbind(Game.update, Game);
-var removeGame = Q.nbind(Game.remove, Game);
+const findGame = Q.nbind(Game.findOne, Game);
+const createGame = Q.nbind(Game.create, Game);
+const updateGame = Q.nbind(Game.update, Game);
+const removeGame = Q.nbind(Game.remove, Game);
 
 export default {
   // Check whether game is in database, if not then create game
-  makeGame: function (req, res, next) {
-    var id = req.body.id;
-    findGame({id: id}).then(function (game) {
+  makeGame(req, res, next) {
+    const id = req.body.id;
+    findGame({ id: id }).then((game) => {
       if (game) {
         res.send(201, game);
       } else {
         createGame({
-          id: id
+          id: id,
         })
-        .then(function (newGame) {
+        .then((newGame) => {
           console.log(newGame);
           res.send(201, newGame._id);
         });
@@ -28,28 +28,28 @@ export default {
   },
 
   // Retrieve game instance from database
-  getGame: function (gameId, res, next) {
-    findGame({id: gameId}).then(function (game) {
+  getGame(gameId, res, next) {
+    findGame({id: gameId}).then((game) => {
       res.send(201, game);
     });
   },
 
   // Update specified field for a given game instance
-  updateGame: function (req, res, next) {
-    var id = req.body.id;
-    var field = req.body.field;
-    var updateQuery = {$set: {}};
+  updateGame(req, res, next) {
+    const id = req.body.id;
+    const field = req.body.field;
+    const updateQuery = {$set: {}};
     updateQuery.$set[field] = true;
-    updateGame({id: id}, updateQuery).then(function (game) {
+    updateGame({ id: id }, updateQuery).then((game) => {
       res.send(201, game);
     });
   },
 
   // Remove specified game instance from database
-  removeGame: function (req, res, next) {
-    var id = req.body.id;
-    removeGame({id: id}).then(function (game) {
+  removeGame(req, res, next) {
+    const id = req.body.id;
+    removeGame({ id: id }).then((game) => {
       res.send(201, game);
     });
-  }
+  },
 };
