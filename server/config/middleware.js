2c359abf7e416836122126c5a288a33c243dd9c3
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-03-02 10:57:30 -0800

    Fully refactor and lint middleware.js using ES6 syntax

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -3,9 +3,9 @@ import bodyParser from 'body-parser';
 
 const middleware = (app, express) => {
   app.use(morgan('dev'));
-  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
-  app.use(express.static(__dirname + '/../../client'));
+  app.use(express.static(`${__dirname}/../../client`));
 };
 
 export default middleware;
